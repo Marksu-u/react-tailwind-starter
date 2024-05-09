dde2e532822dbbf444dd84842c74be134a265a88
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2024-05-09 14:48:49 +0200

    React + TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <div className="bg-gray-200 p-4">
+    <h1 className="text-3xl font-bold text-gray-800">Website</h1>
+    <p className="text-gray-600">Hello World</p>
+  </div>
+);
+
+export default App;
