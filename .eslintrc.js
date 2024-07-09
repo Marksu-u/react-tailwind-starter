cb228f5ff2e5f81a65bafcf869b62302a3566b9e
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2024-07-09 16:18:58 +0200

    Add ESLint, Prettier, and VS Code settings configurations.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'es5',
+        semi: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const App = () => (
-  <div className="bg-gray-200 p-4">
-    <h1 className="text-3xl font-bold text-gray-800">Website</h1>
-    <p className="text-gray-600">Hello World</p>
+  <div className='bg-gray-200 p-4'>
+    <h1 className='text-3xl font-bold text-gray-800'>Website</h1>
+    <p className='text-gray-600'>Hello World</p>
   </div>
 );
 
